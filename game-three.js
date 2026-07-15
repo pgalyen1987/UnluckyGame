95f39459747d1d495658782f46b2df3ad061cb75
pgalyen1987 <pgalyen1987@users.noreply.github.com>
2026-07-15 21:17:53 +0000

    deploy: 5ca8244e1468ea08ae65587bdbf3dc0e5db9c8e9

---
+++ game-three.js
@@ -0,0 +1,2 @@
+import"./modulepreload-polyfill.js";import"./config.js";import{UnluckyThree as e}from"./UnluckyThree.js";import"./three.js";function t(){const t=document.getElementById("game-container");t&&new e(t)}"loading"===document.readyState?document.addEventListener("DOMContentLoaded",t):t();
+//# sourceMappingURL=game-three.js.map
